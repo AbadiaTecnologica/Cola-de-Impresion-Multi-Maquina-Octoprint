4ad3311b2055d9099f340c3b01d78213cc21cf75
adrivarona <adrivarona@gmail.com>
2018-10-05 13:39:08 +0200

    Funciones de Insercion Basicas(Todos los elementos de la tabla)

--- code/baseDatos.py
+++ code/baseDatos.py
@@ -1,4 +1,6 @@
 import mysql.connector
+
+#Init
 mydb=  mysql.connector.connect(
     host="HOST",
     user="USER",
@@ -9,6 +11,7 @@ mydb=  mysql.connector.connect(
 
 mycursor = mydb.cursor(prepared=True)
 
+#Utiles
 def imprimeColumnas(nombre):
     mycursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = %s", (nombre,))
     for x in mycursor:
@@ -23,12 +26,35 @@ def reiniciaTabla(nombre):
     mycursor.execute("DELETE FROM "+nombre+";")
     mycursor.execute("ALTER TABLE "+nombre+" AUTO_INCREMENT = 1")
 
+#Inserts
 def nuevaBoquilla(tamano):
     sql = "INSERT INTO Boquilla (Tamaño) VALUES (%s);"
     val=(tamano,)
     mycursor.execute(sql,val)
     mydb.commit()
 
-reiniciaTabla("Boquilla")
-nuevaBoquilla(17)
-imprimeTabla("Boquilla")
+def nuevoPedido(dueno,prioridad):
+    sql = "INSERT INTO Pedido (Cliente,Prioridad) VALUES (%s,%s);"
+    val = (dueno,prioridad)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+def nuevoTrabajo(idPedido,idBoquilla,idRollo,idMaquina,volumen,Fecha):
+    sql = "INSERT INTO Trabajo (idPedido,idBoquilla,idRollo,idMaquina,volumen,Fecha) VALUES (%s,%s);"
+    val = (idPedido,idBoquilla,idRollo,idMaquina,volumen,Fecha)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+def nuevoRollo(color,t_Impresion,peso):
+    sql = "INSERT INTO Rollo (Color,T_Impresion,Peso) VALUES (%s,%s,%s);"
+    val = (color,t_Impresion,peso)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+def nuevaMaquina(idBoquilla,idRollo,estado):
+    sql = "INSERT INTO Maquina (idBoquilla,idRollo,Estado) VALUES (%s,%s,%s);"
+    val = (idBoquilla, idRollo, estado)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+imprimeColumnas("Maquina")
